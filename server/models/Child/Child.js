80d844c4dff96da9d7472dbfb2ca92cba2bc5578
Yoon Lee <himynameisyoon@gmail.com>
2019-01-26 13:32:49 -0800

    add age field

--- server/models/Child/Child.js
+++ server/models/Child/Child.js
@@ -8,6 +8,7 @@ const FEMALE = 'F';
 const childSchema = new Schema({
   firstName: { type: String, required: true, trim: true },
   lastName: { type: String, required: true, trim: true },
+  age: { type: Number, min: 0, max: 100, required: true },
   gender: { type: String, enum: [MALE, FEMALE], required: true },
   condition: { type: String, required: true, trim: true },
   story: { type: String, trim: true },
