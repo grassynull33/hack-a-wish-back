41a5a3ba07c2f29e8a7ca098e0fc0159ebcb3df5
Yoon Lee <himynameisyoon@gmail.com>
2019-01-26 10:38:44 -0800

    install mongodb

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
 // common utils
 const logger = require('morgan');
+const Promise = require('bluebird');
 
 // Express dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+
 require('dotenv').load();
 
 const app = express();
 const PORT = 5000;
+let db;
 
 /* eslint-disable no-console */
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI, {
+  promiseLibrary: Promise,
+  useNewUrlParser: true,
+});
+
+// eslint-disable-next-line prefer-const
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Mongoose connection successful.');
+});
 
 /** *********************************** */
 //* ****// Express & Webpack Middleware
@@ -37,4 +54,5 @@ require('./routes')(app);
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running at :${PORT}`);
+  console.log(`Mongo URI: ${process.env.MONGODB_URI}`);
 });
