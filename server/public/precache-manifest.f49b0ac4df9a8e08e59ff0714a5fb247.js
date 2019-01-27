a82cd81a8548f25a3d90469b00b368dddb5e32b5
Yoon Lee <himynameisyoon@gmail.com>
2019-01-27 01:17:43 -0800

    add built frontend

---
+++ server/public/precache-manifest.f49b0ac4df9a8e08e59ff0714a5fb247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fce9ef2253eebb21211",
+    "url": "/static/css/main.74aabd37.chunk.css"
+  },
+  {
+    "revision": "3fce9ef2253eebb21211",
+    "url": "/static/js/main.3fce9ef2.chunk.js"
+  },
+  {
+    "revision": "0359da88bbd63e2cd38b",
+    "url": "/static/css/1.0508e7cb.chunk.css"
+  },
+  {
+    "revision": "0359da88bbd63e2cd38b",
+    "url": "/static/js/1.0359da88.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ad8bd18347495068e6fed46d757b4c7",
+    "url": "/index.html"
+  }
+];
